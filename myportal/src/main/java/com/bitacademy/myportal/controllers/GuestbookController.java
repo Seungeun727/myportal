73b7e71f5357a0a066c8354c044bb7ecf3f614f0
노승은 <seun2230@gmail.com>
2021-09-30 13:22:01 +0900

    방명록 리스트 구현: 수정 완료

--- myportal/src/main/java/com/bitacademy/myportal/controllers/GuestbookController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controllers/GuestbookController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bitacademy.myportal.repository.GuestbookVo;
 import com.bitacademy.myportal.service.GuestbookService;
@@ -22,5 +24,11 @@ public class GuestbookController {
 		System.out.println(list);
 		model.addAttribute("list", list);
 		return "guestbook/list"; //  #목록 데이터 전달
+	}
+	
+	@RequestMapping(value="/write", method=RequestMethod.POST)
+	public String write(@ModelAttribute GuestbookVo vo) {
+		System.out.println("FORM DATA:" + vo);
+		return null;
 	}
 }
--- myportal/src/main/webapp/WEB-INF/views/includes/navigation.jsp
+++ myportal/src/main/webapp/WEB-INF/views/includes/navigation.jsp
@@ -1,7 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%-- #태그 라이브러리---%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="C" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 		<!-- navigation -->
 		<div id="navigation">
 			<ul>
