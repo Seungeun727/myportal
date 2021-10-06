0f54b9523542298e2c0b8613672f2b0173d26434
노승은 <seun2230@gmail.com>
2021-10-06 10:35:14 +0900

    사용자 인증을 Interceptor에 위임 ( 다시 이해 )

--- myportal/src/main/java/com/bitacademy/myportal/controllers/BoardController.java
+++ myportal/src/main/java/com/bitacademy/myportal/controllers/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bitacademy.myportal.repository.BoardVo;
+import com.bitacademy.myportal.repository.GuestbookVo;
 import com.bitacademy.myportal.repository.UserVo;
 import com.bitacademy.myportal.service.BoardService;
 
@@ -32,11 +33,11 @@ public class BoardController {
 	@RequestMapping(value="/write", method=RequestMethod.GET)
 	public String writeForm(HttpSession session) {
 		//	로그인 사용자 확인
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if (authUser == null) {
-			System.err.println("로그인 사용자가 아님!");
-			return "redirect:/";
-		}
+//		UserVo authUser = (UserVo)session.getAttribute("authUser");
+//		if (authUser == null) {
+//			System.err.println("로그인 사용자가 아님!");
+//			return "redirect:/";
+//		}
 
 		return "board/write";
 	}
@@ -45,10 +46,11 @@ public class BoardController {
 	@RequestMapping(value="/write", method=RequestMethod.POST)
 	public String write(@ModelAttribute BoardVo boardVo, 
 						HttpSession session) {
+		// 로그인 사용자 확인 -> 인터셉터에 처리 위임
 		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if (authUser == null) {
-			return "redirect:/";
-		}
+//		if (authUser == null) {
+//			return "redirect:/";
+//		}
 		//	작성자 정보 추가
 		boardVo.setUserNo(authUser.getNo());
 		//	삽입
@@ -56,4 +58,18 @@ public class BoardController {
 
 		return "redirect:/board/list";
 	}
+	
+	// 게시물 수정
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public String updateForm(@ModelAttribute BoardVo vo) {
+		boolean updateSucess = boardServiceImpl.update(vo);
+		// 만약 업데이트 수정이 완료되었다면 저장
+		if (updateSucess) {
+			return "redirect:/board" + vo.getNo();
+		}
+		// 실패시 돌아감
+		return "redirect:/";
+	}
+	
+	
 }
